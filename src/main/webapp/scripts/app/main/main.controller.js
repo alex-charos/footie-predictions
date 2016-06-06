0344abcea5af803c71be538ff41a0ac3ad9f5a0f
Alex <realtowz@yahoo.com>
2016-06-06 15:20:54 +0300

    avatar

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,21 +1,34 @@
 'use strict';
 
 angular.module('footierepoApp')
-    .controller('MainController', function ($scope, Principal, Fixture, Prediction) {
+    .controller('MainController', function ($scope, Principal, Fixture, Prediction, User) {
 
 
     	$scope.fixtures = [];
     	$scope.predictions = [];
-    	Fixture.query(function(data) {
-    			$scope.fixtures = data;
-    			Prediction.query(function(predData) {
-    				$scope.predictions = predData;
-    				
-    			})
+    	$scope.users = []
+    	User.query(function(userData) {
+    		$scope.users =userData;
+			Fixture.query(function(data) {
+	    			$scope.fixtures = data;
+	    			Prediction.query(function(predData) {
+	    				$scope.predictions = predData;
 
-    	});
+	    			})
+
+	    	});
+		});
 
 
+    	$scope.getAvatarByUsername = function(username) {
+    		for (var i = 0; i < $scope.users.length; i++) {
+    			if ($scope.users[i].username === username) {
+    				return $scope.users[i].avatar;
+    			}
+    		}
+
+    		return "";
+    	}
 
 
        
